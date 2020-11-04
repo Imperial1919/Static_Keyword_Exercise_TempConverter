5576e53387545bc0500877d6bed8872044d04ae0
Bryce <ImperialEagle1998@gmail.com>
2020-11-03 19:26:45 -0600

    First Version of this.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Human! I am the Tempreture Conversion System or TCS.\n If you would like to convert Farenheit to Celsius, type in f,\n if you would wish to convert Celsius to farenheit, type in c.");
+            string userinput = Console.ReadLine().ToLower();
+            Console.WriteLine("And the value You would like to convert?");
+            string value = Console.ReadLine();
+            double convertedvalue = Double.Parse(value);
+
+           if(userinput == "f")
+            {
+                 TempConverter.FahrenheittoCelcius(convertedvalue);
+            }
+
+           if(userinput == "c")
+            {
+                TempConverter.CelciustoFahrenheit(convertedvalue);
+            }
+        }
+    }
+}
---
+++ StaticExercise/tempconverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+
+
+        public static double FahrenheittoCelcius(double temp)
+        {
+            double f = 32;
+            double c = 32;
+
+            double answer = f - c * 5 / 9;
+            Console.WriteLine($"{temp} degrees celsius is equal to {answer} degrees Fahrenheit");
+
+            return answer;
+
+
+        }
+
+
+
+        public static double CelciustoFahrenheit(double temp)
+        {
+            double f = 32;
+            double c = 0;
+
+            double answer = c * 9 / 5 + 32;
+            Console.WriteLine($"{temp} degrees celsius is equal to {answer} degrees Fahrenheit");
+            
+            return answer;
+        }
+    }
+}
